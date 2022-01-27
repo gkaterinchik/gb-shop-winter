5007f30b729250232cc5442d3615f014f1074919
G.Katerinchik <ggg140@yandex.ru>
2022-01-27 14:10:09 +0300

    leson_3_katerinchik изменен путь к gb-setting

--- config-server/src/test/java/ru/gb/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/ru/gb/configserver/ConfigServerApplicationTests.java
@@ -1,6 +1,7 @@
 package ru.gb.configserver;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
